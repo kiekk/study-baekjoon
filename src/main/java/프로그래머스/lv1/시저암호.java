1423943ed1444d35de7e58e308cd7c7b85b1c07d
soonho <shyoon991@gmail.com>
2023-09-06 16:36:05 +0900

    프로그래머스 문제 - 시저 암호 - 개선

--- src/main/java/프로그래머스/lv1/시저암호.java
+++ src/main/java/프로그래머스/lv1/시저암호.java
@@ -5,21 +5,19 @@ import java.util.stream.Collectors;
 
 public class 시저암호 {
     public String solution(String s, int n) {
-        String answer = Arrays.stream(s.split(""))
-                .map(spelling -> {
-                    char ascii = spelling.charAt(0);
-
-                    if (ascii >= 65 && ascii <= 90) {
-                        return ascii + n > 90 ? String.valueOf((char) (ascii + n - 26)) : String.valueOf((char) (ascii + n));
+        return s.chars()
+                .map(character -> {
+                    if (Character.isUpperCase(character)) {
+                        return character + n > 'Z' ? character + n - 26 : character + n;
                     }
 
-                    if (ascii >= 97 && ascii <= 122) {
-                        return ascii + n > 122 ? String.valueOf((char) (ascii + n - 26)) : String.valueOf((char) (ascii + n));
+                    if (Character.isLowerCase(character)) {
+                        return character + n > 'z' ? character + n - 26 : character + n;
                     }
 
-                    return " ";
+                    return character;
                 })
+                .mapToObj(character -> String.valueOf((char) character))
                 .collect(Collectors.joining());
-        return answer;
     }
 }
