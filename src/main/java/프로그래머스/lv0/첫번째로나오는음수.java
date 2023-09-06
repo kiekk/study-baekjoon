d429ad584a49b331f037feb73f24b7a41ded6ae3
soonho <shyoon991@gmail.com>
2023-09-07 08:58:30 +0900

    프로그래머스 문제 - 첫 번째로 나오는 음수

---
+++ src/main/java/프로그래머스/lv0/첫번째로나오는음수.java
@@ -0,0 +1,14 @@
+package 프로그래머스.lv0;
+
+public class 첫번째로나오는음수 {
+    public int solution(int[] num_list) {
+        int answer = -1;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) {
+                answer = i;
+                break;
+            }
+        }
+        return answer;
+    }
+}
