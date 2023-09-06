014f981dc5779f9edc7771b3aac90a8f6dc7d7e7
soonho <shyoon991@gmail.com>
2023-09-06 15:41:23 +0900

    프로그래머스 문제 - 문자열 내림차순으로 배치하기 - 개선

--- src/main/java/프로그래머스/lv1/문자열내림차순으로배치하기.java
+++ src/main/java/프로그래머스/lv1/문자열내림차순으로배치하기.java
@@ -1,21 +1,13 @@
 package 프로그래머스.lv1;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class 문자열내림차순으로배치하기 {
     public String solution(String s) {
-        String answer = "";
-
-        List<String> strList = new ArrayList<>();
-        for (int i = 1; i <= s.length(); i++) {
-            strList.add(s.substring(i - 1, i));
-        }
-
-        strList.sort(Comparator.reverseOrder());
-
-        answer = String.join("", strList);
-        return answer;
+        return Arrays.stream(s.split(""))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining());
     }
 }
