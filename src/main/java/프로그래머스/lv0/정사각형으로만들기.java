02592bd5da04d7a2acd3f8151740231b9b7d9471
soonho <shyoon991@gmail.com>
2023-09-09 07:38:47 +0900

    프로그래머스 문제 - 정사각형으로 만들기 - 개선

--- src/main/java/프로그래머스/lv0/정사각형으로만들기.java
+++ src/main/java/프로그래머스/lv0/정사각형으로만들기.java
@@ -1,27 +1,13 @@
 package 프로그래머스.lv0;
 
-import java.util.Arrays;
-
 public class 정사각형으로만들기 {
     public int[][] solution(int[][] arr) {
-        int[][] answer = {};
-        int length = arr.length;
+        int maxLength = Math.max(arr.length, arr[0].length);
+        int[][] answer = new int[maxLength][maxLength];
 
-        for (int[] ar : arr) {
-            length = Math.max(length, ar.length);
+        for (int i = 0; i < arr.length; i++) {
+            System.arraycopy(arr[i], 0, answer[i], 0, arr[0].length);
         }
-
-        answer = new int[length][length];
-
-        for (int i = 0; i < answer.length; i++) {
-
-            if (i >= arr.length) {
-                answer[i] = new int[length];
-            } else {
-                answer[i] = Arrays.copyOf(arr[i], length);
-            }
-        }
-
         return answer;
     }
 }
